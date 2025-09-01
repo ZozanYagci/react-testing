270bb2cd0e46915b52bbbd14d5dca817e2444679
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-09-02 00:04:29 +0300

    mock fetch in UserProfile test for reliability

--- src/tests/UserProfile.test.jsx
+++ src/tests/UserProfile.test.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import UserProfile from "../components/UserProfile";
 import "@testing-library/jest-dom/vitest";
 
 describe("UserProfile", () => {
+  beforeEach(() => {
+    global.fetch = vi.fn();
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   it("fetch data and display the user data", async () => {
+    global.fetch.mockResolvedValueOnce({
+      json: async () => ({
+        id: 4,
+        name: "HomoSapiens",
+        email: "sapiens@gmail.com",
+      }),
+    });
+
     render(<UserProfile userId={1} />);
 
     expect(screen.getByText(/loading/i)).toBeInTheDocument();
 
     await waitFor(() => {
       expect(
-        screen.getByRole("heading", { name: /leanne/i })
+        screen.getByRole("heading", { name: /HomoSapiens/i })
       ).toBeInTheDocument();
 
-      expect(screen.getByText(/sincere@april.biz/i)).toBeInTheDocument();
+      expect(screen.getByText(/sapiens@gmail.com/i)).toBeInTheDocument();
     });
   });
 });
